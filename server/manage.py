a9e3195e34886d57eec3fe9b5da0e30c65cceb83
Jean-Marc Wagner <jeanmarc.wagner@gmx.de>
2019-06-10 20:22:15 +0200

    added create_admin function

--- server/manage.py
+++ server/manage.py
@@ -3,15 +3,34 @@ from flask.cli import FlaskGroup
 from app import create_app
 from app.database import db
 
+from app.api.user.models import User
 
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 prompt = "> "
 
+
 @cli.command('create_admin')
 def create_admin_user():
-    pass
+    if not User.query.filter_by(is_admin=True).first():
+        print("Username: ")
+        username = input(prompt)
+        print("Email: ")
+        email = input(prompt)
+        print("Password: ")
+        pw1 = input(prompt)
+        print("Password repeat: ")
+        pw2 = input(prompt)
+
+        if pw1 == pw2:
+            user = User(username, pw1, email)
+            user.is_admin = True
+            user.save()
+            print("Admin user created")
+        else:
+            print("Admin user already exists")
+
 
 if __name__ == '__main__':
     cli()
