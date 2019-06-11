382fa78473bc37199b142bde0aa14ace31c9cd10
Jean-Marc Wagner <jeanmarc.wagner@gmx.de>
2019-06-11 20:21:21 +0200

    mit den stammdaten resourcen angefangen fur sentosa

--- server/app/api/stammdaten/resources.py
+++ server/app/api/stammdaten/resources.py
@@ -1,2 +1,40 @@
 from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.api.stammdaten.models import SentosaSetting, SentosaUntersuchung, SentosaSettingSchema, SentosaUntersuchungSchema
+
+
+class SentosaSettingListApi(Resource):
+    def get(self):
+        response = {}
+        sentosaSettings = SentosaSetting.query.get(1)
+        if sentosaSettings.is_active:
+            schema = SentosaSettingSchema()
+            response['status'] = "OK"
+            response['sentosaSettings'] =  schema.dump(sentosaSettings).data
+            return response, 200
+
+        else:
+            response['status'] = "NOT ACTIVE"
+            response['message'] = "Keine aktive Einstellung gefunden"
+
+            return response, 404
+
+class SentosaSettingApi(Resource):
+    def put(self, sentSett_id):
+        pass
+
+class SentosaUntersuchungListApi(Resource):
+    def get(self):
+        schema = SentosaUntersuchungSchema(many=True)
+        sentosaUntersuchungen = SentosaUntersuchung.query.filter_by(
+            sentosaSetting_id=1).first()
+        return schema.dump(sentosaUntersuchungen).data, 200
+        
+    
+    def post(self):
+        pass
+
+class SentosaUntersuchungApi(Resource):
+    def put(self, sentUnters_id):
+        pass
